5a839e9dd2faa6f0090dc2d78b1aa64821cc4576
Stanislav Matas <stanislav.matas@gmail.com>
2021-04-24 20:03:08 +0200

    add help text to image field

--- catalog/models.py
+++ catalog/models.py
@@ -97,7 +97,7 @@ class Course(models.Model):
     name = models.CharField(_('název'), max_length=50, blank=False)
     slug = AutoSlugField(_('slug'), populate_from='name')  # make unique with organization?
     description = models.TextField(_('popis'), blank=True)
-    image = models.ImageField(_('obrázek'), upload_to=image_directory_path)
+    image = models.ImageField(_('obrázek'), upload_to=image_directory_path, help_text='minimální rozměr 500x500 px')
     price = models.PositiveIntegerField(_('cena za kurz'), null=False, blank=False)
     hours = models.PositiveIntegerField(_('počet hodin'), null=False, blank=False, validators=[MinValueValidator(1)])
     capacity = models.PositiveIntegerField(_('maximální kapacita'), null=True, blank=True)
