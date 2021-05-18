e052de0bf04c458e264d3b665f504b300bd27459
Stanislav Matas <stanislav.matas@gmail.com>
2021-05-18 10:31:39 +0200

    write tests for new visitor

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,7 +1,19 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import resolve
+from .views import home
 
 
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Vyber</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,7 @@ import unittest
 from time import time, sleep
 
 from helium import S, Config
-from helium import start_firefox, go_to
+from helium import start_firefox, go_to, get_driver, scroll_down, kill_browser
 from selenium.webdriver.remote.webelement import WebElement
 
 
@@ -52,17 +52,58 @@ def get_element(sel: str, limit: int = 10) -> WebElement:
 
 # run with a local interpreter, not within docker
 class NewVisitorTest(unittest.TestCase):
+    """Test whether a new user with no account can open any page that does not require an account."""
 
     def setUp(self) -> None:
-        self.browser = start_firefox()
+        start_firefox(headless=True)
+        go_to('http://0.0.0.0:8000/')
+        get_element('//a[@id="djHideToolBarButton"]').click()
 
     def tearDown(self) -> None:
-        self.browser.quit()
+        get_driver().quit()
+        kill_browser()
 
-    def test_can_open_homepage_and_see_header(self):
-        go_to('http://0.0.0.0:8000/')
-        self.assertIn('Vyber online aktivitu', self.browser.title)
-        # self.fail('Finish the test!')
+    def test_can_open_homepage_and_see_header_and_lead_text(self):
+        self.assertIn('Vyber online aktivitu', get_driver().title)
+        lead_text = get_element('h1.display-1').text
+        self.assertIn('Vyhledávač online aktivit', lead_text)
+
+    def test_can_open_search_page_via_btn_homepage(self):
+        get_element('//a[@class="btn btn-lg btn-primary"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Vybrat', lead_text)
+
+    def test_can_open_search_page_via_navbar_homepage(self):
+        get_element('//a[@href="/hledani/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Vybrat', lead_text)
+
+    def test_can_open_catalog_page_via_homepage(self):
+        get_element('//a[@href="/aktivity/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Katalog', lead_text)
+
+    def test_can_open_conditions_page_via_homepage(self):
+        scroll_down(3000)
+        get_element('//a[@href="/podminky-uzivani/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Podmínky užívání', lead_text)
+
+    def test_can_open_gdpr_page_via_homepage(self):
+        scroll_down(3000)
+        get_element('//a[@href="/gdpr/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Zásady ochrany osobních údajů', lead_text)
+
+    def test_can_open_about_page_via_homepage(self):
+        get_element('//a[@href="/o-nas/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('O nás', lead_text)
+
+    def test_can_open_cooperation_page_via_homepage(self):
+        get_element('//a[@href="/spoluprace/"]').click()
+        lead_text = get_element('h1').text
+        self.assertIn('Spolupráce', lead_text)
 
 
 if __name__ == '__main__':
