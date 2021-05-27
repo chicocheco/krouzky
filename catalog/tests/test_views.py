e4f12dbd3ef7326aaece13e0e5a719ae2ba76ed2
Stanislav Matas <stanislav.matas@gmail.com>
2021-05-27 17:01:32 +0200

    add few view tests of course model

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,95 +1,54 @@
-from django.test import TestCase, SimpleTestCase
+from datetime import timedelta, date
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management import call_command
+from django.test import TestCase
 from django.urls import reverse
 from django.utils.text import slugify
 
-from catalog.models import Organization
+from catalog.models import Organization, Course, AgeCategory
 from users.models import User
 
 
-# class ListViews(TestCase):
-#
-#     def setUp(self) -> None:
-#         self.new_user = User.objects.create_user(email='lofy@seznam.cz', password='heslo123')
-#         self.new_organization = Organization.objects.create(name='Moje Organizace',
-#                                                             url='https://www.adresa.cz',
-#                                                             company_id=98765432,
-#                                                             address='Moje adresa 987',
-#                                                             town='Praha',
-#                                                             zip_code=77722)
-#         self.age_category = AgeCategory.objects.create(name='Větší školáci',
-#                                                        age_from=11,
-#                                                        age_to=13)
-#         WeekSchedule.fill_table()
-#         # TODO: price, hours, capacity, teacher, organization, age_category,
-#         # TODO: status,
-#         self.new_course = Course.objects.create(name='Moje aktivita',
-#                                                 description='Mǔj popis aktivity',
-#                                                 url='https://www.adresa-aktivity.cz',
-#                                                 category=Course.Category.OTHER,
-#                                                 image=SimpleUploadedFile(name='test_image_new.jpg',
-#                                                                          content=open('static/img/test_image.jpg',
-#                                                                                       'rb').read(),
-#                                                                          content_type='image/jpeg'),
-#                                                 price=2000,
-#                                                 hours=100,
-#                                                 capacity=10,
-#                                                 teacher=self.new_user,
-#                                                 organization=self.new_organization,
-#                                                 age_category=self.age_category,
-#                                                 status=Course.Status.PUBLISHED,
-#                                                 date_from=datetime.now() + timedelta(days=1),
-#                                                 date_to=datetime.now() + timedelta(days=2),
-#                                                 )
-#
-#         self.new_course2 = Course.objects.create(name='Moje další aktivita',
-#                                                  description='Mǔj další popis aktivity',
-#                                                  url='https://www.adresa-aktivity.cz',
-#                                                  category=Course.Category.OTHER,
-#                                                  image=SimpleUploadedFile(name='test_image_new2.jpg',
-#                                                                           content=open('static/img/test_image.jpg',
-#                                                                                        'rb').read(),
-#                                                                           content_type='image/jpeg'),
-#                                                  price=2000,
-#                                                  hours=100,
-#                                                  capacity=10,
-#                                                  teacher=self.new_user,
-#                                                  organization=self.new_organization,
-#                                                  age_category=self.age_category,
-#                                                  status=Course.Status.PUBLISHED,
-#                                                  date_from=datetime.now() + timedelta(days=1),
-#                                                  date_to=datetime.now() + timedelta(days=2),
-#                                                  )
+class StaticPagesTests(TestCase):
 
-# def test_search_GET(self):
-#     self.new_organization.slug = slugify(self.new_organization.name)
-#     self.new_organization.save()
-#     response = self.client.get(reverse('search'))
-#     self.assertEqual(response.status_code, 200)
-#     self.assertTemplateUsed(response, 'catalog/course/search.html')
-#
-# def test_course_list_GET(self):
-#     response = self.client.get(reverse('course_list'))
-#     self.assertEqual(response.status_code, 200)
-#     self.assertTemplateUsed(response, 'catalog/course/list.html')
-#
-# def test_course_list_by_organization_GET(self):
-#     url = reverse('course_list_by_organization', args=('some-org',))
-#     response = self.client.get(url)
-#     self.assertEqual(response.status_code, 200)
-#     self.assertTemplateUsed(response, 'catalog/course/list_organization.html')
+    def test_home_GET(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/home.html')
+
+    def test_gdpr_GET(self):
+        response = self.client.get(reverse('gdpr'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/gdpr.html')
+
+    def test_conditions_GET(self):
+        response = self.client.get(reverse('conditions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/conditions.html')
+
+    def test_cooperation_GET(self):
+        response = self.client.get(reverse('cooperation'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/cooperation.html')
+
+    def test_about_GET(self):
+        response = self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/about.html')
 
 
 class OrganizationTests(TestCase):
-    data = {'name': 'Nová organizace',
-            'url': 'https://www.nova-organizace.cz',
-            'company_id': 98765443,
-            'address': 'Moje adresa 123',
-            'town': 'Praha',
-            'zip_code': 33322}
 
-    def setUp(self) -> None:
+    def setUp(self):
         self.new_user = User.objects.create_user(email='lofy@seznam.cz', password='heslo123')
         self.client.login(email='lofy@seznam.cz', password='heslo123')
+        self.data = {'name': 'Nová organizace',
+                     'url': 'https://www.nova-organizace.cz',
+                     'company_id': 98765443,
+                     'address': 'Moje adresa 123',
+                     'town': 'Praha',
+                     'zip_code': 33322}
 
     def test_organization_register_uses_correct_template_GET(self):
         url = reverse('organization_register')
@@ -138,7 +97,7 @@ class OrganizationTests(TestCase):
 
         response = self.client.post(url, self.data)
 
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(Organization.objects.count(), 1)
 
     def test_organization_register_creates_slug_from_name_POST(self):
@@ -156,8 +115,8 @@ class OrganizationTests(TestCase):
 
         new_organization = Organization.objects.first()
         self.new_user.refresh_from_db()  # necessary because of "request.user" modifications
-        self.assertEquals(self.new_user.role, User.Roles.COORDINATOR)
-        self.assertEquals(self.new_user.organization, new_organization)
+        self.assertEqual(self.new_user.role, User.Roles.COORDINATOR)
+        self.assertEqual(self.new_user.organization, new_organization)
 
     # updating
     def test_organization_update_uses_users_organization_GET(self):
@@ -222,40 +181,75 @@ class OrganizationTests(TestCase):
         self.client.post(url2)
         self.new_user.refresh_from_db()  # necessary because of "request.user" modifications
 
-        self.assertEquals(self.new_user.role, User.Roles.STUDENT)
+        self.assertEqual(self.new_user.role, User.Roles.STUDENT)
 
 
-class StaticPagesTests(SimpleTestCase):
+class CourseTests(TestCase):
 
-    def test_home_GET(self):
-        response = self.client.get(reverse('home'))
+    def setUp(self):
+        call_command('flush', '--no-input')  # else it does clear db between tests
+        self.new_user = User.objects.create_user(email='lofy@seznam.cz', password='heslo123')
+        self.client.login(email='lofy@seznam.cz', password='heslo123')
+        # do what organization_register view does here:
+        organization_name = 'Moje Organizace'
+        self.new_organization = Organization.objects.create(name=organization_name,
+                                                            slug=slugify(organization_name),
+                                                            url='https://www.adresa.cz',
+                                                            company_id=98765432,
+                                                            address='Moje adresa 987',
+                                                            town='Praha',
+                                                            zip_code=77722)
+        self.new_user.organization = self.new_organization
+        self.new_user.role = User.Roles.COORDINATOR
+        self.new_user.save()
+
+        self.age_category = AgeCategory.objects.create(name='Větší školáci',
+                                                       age_from=11,
+                                                       age_to=13)
+
+        self.data_regular = {'name': 'Název pravidelné SUPER aktivity',
+                             'url': 'https://www.organizace.cz/pravidelna-aktivita',
+                             'tags': 'pokus-tag',
+                             'category': 'OTHER',  # predefined in Course model
+                             'description': 'Mǔj popis pravidelné aktivity',
+                             'image': SimpleUploadedFile(name='test_image_new.jpg',
+                                                         content=open('static/img/test_image.jpg',
+                                                                      'rb').read(),
+                                                         content_type='image/jpeg'),
+                             'price': 2000,
+                             'hours': 100,
+                             'capacity': 10,
+                             'teacher': 1,  # use first
+                             'age_category': 1,  # use first
+                             'date_from': date.today() + timedelta(days=1),
+                             'date_to': date.today() + timedelta(days=2),
+                             }
+
+    def test_course_create_uses_correct_template_GET(self):
+        url = reverse('course_create')
+
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/home.html')
+        self.assertTemplateUsed(response, 'catalog/course/create.html')
 
-    def test_gdpr_GET(self):
-        response = self.client.get(reverse('gdpr'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/gdpr.html')
+    def test_course_create_creates_new_object_POST(self):
+        url = reverse('course_create')
 
-    def test_conditions_GET(self):
-        response = self.client.get(reverse('conditions'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/conditions.html')
+        self.client.post(url, self.data_regular)
 
-    def test_cooperation_GET(self):
-        response = self.client.get(reverse('cooperation'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/cooperation.html')
+        self.assertEqual(Course.objects.count(), 1)
 
-    def test_about_GET(self):
-        response = self.client.get(reverse('about_us'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/about.html')
+    def test_course_create_get_assigned_users_organization_POST(self):
+        url = reverse('course_create')
 
-# class CourseTests(TestCase):
-#
-#     def test_detail_GET(self):
-#         url = self.new_course.get_absolute_url()
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'catalog/course/detail.html')
+        self.client.post(url, self.data_regular)
+        course = Course.objects.first()
+        self.assertEqual(course.organization, self.new_organization)
+
+    def test_course_create_capitalizes_name_POST(self):
+        url = reverse('course_create')
+
+        self.client.post(url, self.data_regular)
+        course = Course.objects.first()
+        self.assertEqual(course.name, self.data_regular['name'].capitalize())
