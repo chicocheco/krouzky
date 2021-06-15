b1dde25111ef12b4aa803479d502bbff0a538982
Stanislav Matas <stanislav.matas@gmail.com>
2021-06-15 19:34:13 +0200

    modify settings for env.var. production again

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,12 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['vyberaktivitu.online'
 
 if ENVIRONMENT == 'production':
     SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'  # allows forms widget customization
 
