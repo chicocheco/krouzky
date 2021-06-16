ec8b9da2cc23946da6f859857499a8028266cbf4
Stanislav Matas <stanislav.matas@gmail.com>
2021-06-16 07:17:00 +0200

    do not serve media files in production with static()

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     # path('ucet/', include('allauth.urls')),  # fallback
     path('', include('catalog.urls')),
     path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:  # local development only
     import debug_toolbar
+
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
